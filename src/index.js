050c270b8e8e884e879252cb49ce8fcf4d023213
bilelBenziane <bilelBenziane@hotmail.com>
2019-07-22 11:12:55 +0100

    passing props from father to child component

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import faker from 'faker';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
+            <CommentDetail author="Dandan"/>
         </div>
     );
 };
