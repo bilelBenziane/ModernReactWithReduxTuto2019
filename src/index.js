b4e196529c282343a646dfaa038e8d35e3c76b48
bilelBenziane <bilelBenziane@hotmail.com>
2019-07-22 10:27:37 +0100

    installing faker and insert fake images and doubling them

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { red } from 'ansi-colors';
-
+import faker from 'faker';
 // Create a react component 
 const App = () => {
     return (
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar"/>
+                    <img alt="avatar" src={faker.image.avatar()}/>
                 </a> 
                 <div className="content">
                     <a href="/" className="author"> 
@@ -21,10 +20,23 @@ const App = () => {
                     <div className="text">Nice blog post!</div>    
                 </div>
             </div>
-
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar"/>
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                </a> 
+                <div className="content">
+                    <a href="/" className="author"> 
+                        sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>    
+                </div>
+            </div>            
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()}/>
                 </a> 
                 <div className="content">
                     <a href="/" className="author"> 
