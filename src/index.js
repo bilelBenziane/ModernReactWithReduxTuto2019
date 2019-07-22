0cacb3f970a8f5f45ba5ecb0675f1d12e0dee80e
bilelBenziane <bilelBenziane@hotmail.com>
2019-07-22 11:00:57 +0100

    extracting boiler plates to a nested component

--- src/index.js
+++ src/index.js
@@ -1,53 +1,13 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
 import faker from 'faker';
 // Create a react component 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a> 
-                <div className="content">
-                    <a href="/" className="author"> 
-                        sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>    
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a> 
-                <div className="content">
-                    <a href="/" className="author"> 
-                        sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>    
-                </div>
-            </div>            
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a> 
-                <div className="content">
-                    <a href="/" className="author"> 
-                        sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>    
-                </div>
-            </div>
+            <CommentDetail />
         </div>
     );
 };
