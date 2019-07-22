b05e09557ec0a9dc2d42a477223e723a66222fe5
bilelBenziane <bilelBenziane@hotmail.com>
2019-07-22 10:12:31 +0100

    adding comments component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { red } from 'ansi-colors';
 
 // Create a react component 
 const App = () => {
     return (
-        <div>
-            <label className="label" for="name">Enter name</label>
-            <input id="name" type="text"/> 
-            <button style={{backgroundColor: 'blue', color:'white'}}>Submit</button>     
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar"/>
+                </a> 
+                <div className="content">
+                    <a href="/" className="author"> 
+                        sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>    
+                </div>
+            </div>
+
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar"/>
+                </a> 
+                <div className="content">
+                    <a href="/" className="author"> 
+                        sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>    
+                </div>
+            </div>
         </div>
     );
 };
