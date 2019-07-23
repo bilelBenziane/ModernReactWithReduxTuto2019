a6ee2c0e9b466e3a3a4898ad458735881b8727f0
bilelBenziane <bilelBenziane@hotmail.com>
2019-07-23 11:30:16 +0100

    adding state properties

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component{
-    render() {
+    constructor(props){
+        super(props);
+        this.state = { lat: null };
+        
         window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
+            position => {
+                this.setState({lat:position.coords.latitude});
+            },
             err => console.log(err)
         );
-        return <div>Latitud : </div>
+    }
+    render() {
+
+        return <div>Latitud : {this.state.lat}</div>
     }
 }
 
