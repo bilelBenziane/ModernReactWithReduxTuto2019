89146b3c532ec5100d0936f81e21257025de6f61
bilelBenziane <bilelBenziane@hotmail.com>
2019-07-23 14:07:07 +0100

    refactoring to event hooks based code

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
 const SeasonDisplay = () => {
-    Window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
     return <div>Ho there !</div>;
 };
 
--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = { lat: null, errorMessage: '' };
+    state = { lat: null, errorMessage: '' };//  this is equivalent to constructor initialized state 
 
+    componentDidMount(){
+        console.log('My component was rendered to the screen');
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({lat:position.coords.latitude});
-            },
-            err => {
-                this.setState({
-                    errorMessage: err.message
-                });
-            }
+            position =>this.setState({lat:position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
         );
     }
+
+    componentDidUpdate(){
+        console.log('My component was just updated');
+    }
+
     render() {
         if(this.state.errorMessage && !this.state.latitude){
             return (
