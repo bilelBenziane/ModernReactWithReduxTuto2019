72fb5cc57ac88074527a5e2c99ffe2ab33aa3f9b
bilelBenziane <bilelBenziane@hotmail.com>
2019-07-23 15:44:17 +0100

    weather app simple and styled

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
 const seasonConfig = {
@@ -24,10 +25,10 @@ const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
     const { text, iconName } = seasonConfig[season]; 
     return (
-        <div>
-            <i className={`${iconName} icon massive`}></i>
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left ${iconName} icon massive`}></i>
             <h1>{text}</h1> 
-            <i className={`${iconName} icon massive`}></i>
+            <i className={`icon-right ${iconName} icon massive`}></i>
         </div>
     );
 };
