28afd9560ad0ab51d8fee40f56684ade0aeb5d38
bilelBenziane <bilelBenziane@hotmail.com>
2019-07-30 15:37:21 +0100

    adding image lists

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const ImageList = (props) =>{
     console.log(props.images);
-    return <div>Image List</div>;
+    const images = props.images.map((image) => {
+        return <img src={image.urls.regular} />
+    });
+    return <div>{images}</div>;
 }
 
 export default ImageList;
