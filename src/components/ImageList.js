91f9e0b9da293f00cf019bfd1c7c2518cddf90b4
bilelBenziane <bilelBenziane@hotmail.com>
2019-07-30 16:03:03 +0100

    refactoring - using destructuring

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ImageList = (props) =>{
     console.log(props.images);
-    const images = props.images.map((image) => {
-        return <img key ={image.id} src={image.urls.regular} alt={image.description}/>
+    const images = props.images.map(({description, id, urls}) => {
+        return <img key ={id} src={urls.regular} alt={description}/>
     });
     return <div>{images}</div>;
 }
