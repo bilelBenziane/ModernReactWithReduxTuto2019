e75f2915fd5a6492aec9978ebf31bc824e24bcfd
bilelBenziane <bilelBenziane@hotmail.com>
2019-07-30 16:00:50 +0100

    adding keys and alt text to images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ImageList = (props) =>{
     console.log(props.images);
     const images = props.images.map((image) => {
-        return <img src={image.urls.regular} />
+        return <img key ={image.id} src={image.urls.regular} alt={image.description}/>
     });
     return <div>{images}</div>;
 }
