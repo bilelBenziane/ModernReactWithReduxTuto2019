53f9e67a18abe9e11a333eae6679c8f5f1b1a137
bilelBenziane <bilelBenziane@hotmail.com>
2019-07-31 10:08:01 +0100

    refactor image to a seperate component ImageCard

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
 import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) =>{
     console.log(props.images);
-    const images = props.images.map(({description, id, urls}) => {
-        return <img key ={id} src={urls.regular} alt={description}/>
+    const images = props.images.map((image) => {
+        return <ImageCard key ={image.id} image={image}/>
     });
     return <div className="image-list">{images}</div>;
 }
